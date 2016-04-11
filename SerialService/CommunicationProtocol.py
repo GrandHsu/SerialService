4add4907b76f8f8e1dabc5c52fcda16bc253e3d0
rsy <mr.iridescent.rsy@hotmail.com>
2016-04-11 17:10:38 +0800

    通信协议模块基本功能编写完成

--- SerialService/CommunicationProtocol.py
+++ SerialService/CommunicationProtocol.py
@@ -1,46 +1,93 @@
 # -*- coding:utf-8 -*-
 
-import struct
-
 FRAME_HEAD = "ttt"
 FRAME_TAIL = "~"
 
-class CommunicationProtocol(object):
-    def __init__(self, *args, **kwargs):
-        return super(CommunicationProtocol, self).__init__(*args, **kwargs)
+FRAME_DATALENGTH = 12
+FRAME_FRAMELENGTH = 8 + FRAME_DATALENGTH * 2
 
-    @staticmethod
-    def frameMake(addr, cmd, data):
-        da = ['0' for _ in range(24)]
-        for i, d in enumerate(data):
-            split = CommunicationProtocol.charSplit(d)
-            da[2 * i] = split[0]
-            da[2 * i + 1] = split[1]
-        check = CommunicationProtocol.frameCheckSum(da)
-        frame = "{0}{1}{2}{3}{4}{5}".format(FRAME_HEAD,chr(addr), chr(cmd), "".join(da), "".join(CommunicationProtocol.charSplit(check)), FRAME_TAIL)
-        return frame
+'''
+制作通信帧
+@addr   - 地址
+@cmd    - 指令
+@data   - 数据
+@return - 帧字符串
+'''
+def frameMake(addr, cmd, data):
+    da = ['0' for _ in range(24)]
+    for i, d in enumerate(data):
+        split = charSplit(d)
+        da[2 * i] = split[0]
+        da[2 * i + 1] = split[1]
+    da = [chr(addr), chr(cmd)] + da
+    check = frameCheckSum(da)
+    frame = "{0}{1}{2}{3}".format(FRAME_HEAD, "".join(da), "".join(charSplit(check)), FRAME_TAIL)
+    return frame
     
-    @staticmethod
-    def charSplit(char):
-        c = ord(char)
-        return '{0}{1}'.format(hex(c / 16)[2:].upper(), hex(c % 16)[2:].upper())
+'''
+二字节拆分 - 拆分
+@char     - 一个字节
+@return   - 拆分后的两个字节
+'''
+def charSplit(char):
+    c = ord(char)
+    return '{0}{1}'.format(hex(c / 16)[2:].upper(), hex(c % 16)[2:].upper())
 
-    @staticmethod
-    def frameCheckSum(data):
-        check = 0
-        for d in data:
-            check = check + ord(d)
-        return chr(check % 256)
+'''
+二字节拆分 - 组合
+@charH    - 高字节
+@charL    - 低字节
+@return   - 组合后的两个字节
+'''
+def charCombine(charH, charL):
+    return chr(int('{0}{1}'.format(charH, charL), base=16))
 
-    @staticmethod
-    def frameCheck(frame):
-        if FRAME_HEAD != frame[0:3]:
-            print "[frameCheck] fail : head not match!"
-            return
+'''
+计算校验和
+@data   - 数据
+@return - 校验和值
+'''
+def frameCheckSum(data):
+    check = 0
+    for d in data:
+        check = check + ord(d)
+    return chr(check % 256)
+
+'''
+检查数据帧
+@frame  - 数据帧
+@return - 返回 地址，指令，数据
+'''
+def frameCheck(frame):
+    ''' e 错误输出 '''
+    e = "[frameCheck] fail: "
+
+    ''' 检查帧长 '''
+    if len(frame) is not FRAME_FRAMELENGTH:
+        print e + "length not match!"
+        return
+    ''' 检查帧头 '''
+    if not frame.startswith(FRAME_HEAD):
+        print e + "head not match!"
+        return
+    ''' 检查帧尾 '''
+    if not frame.endswith(FRAME_TAIL):
+        print e + "tail not match!"
+        return
+    ''' 校验数据 '''
+    if not charCombine(frame[-3], frame[-2]) == frameCheckSum(frame[3:-3]):
+        print e + "data check not correct!"
+        return
         
+    addr = ord(frame[3])
+    cmd  = ord(frame[4])
+    da = frame[5:-3]
+    data = [charCombine(da[2 * i], da[2 * i + 1]) for i in range(len(da)/2)]
+        
+    return addr, cmd, data
 
 
 if __name__ == '__main__':
-    frame = CommunicationProtocol.frameMake(1, 2, "111111111111")
+    frame = frameMake(1, 2, "This is rsy!")
     print frame
-    CommunicationProtocol.frameCheck(frame)
+    print frameCheck(frame)
--- SerialService/serialService.py
+++ SerialService/serialService.py
@@ -7,6 +7,13 @@ import time
 import serial
 import serial.tools.list_ports
 
+import CommunicationProtocol as cp
+
 class SerialService(object):
     def __init__(self):
         pass
+
+if __name__ == "__main__":
+    frame = cp.frameMake(1, 2, "This is rsy!")
+    print frame
+    print cp.frameCheck(frame)
