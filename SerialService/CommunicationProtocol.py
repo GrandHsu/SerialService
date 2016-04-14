5f14f265607bc8fc05656c0583b4fac985b1cae5
rsy <mr.iridescent.rsy@hotmail.com>
2016-04-14 09:59:59 +0800

    修改注释，优化赋值结构

--- SerialService/CommunicationProtocol.py
+++ SerialService/CommunicationProtocol.py
@@ -6,14 +6,8 @@ FRAME_TAIL = "~"
 FRAME_DATALENGTH = 12
 FRAME_FRAMELENGTH = 8 + FRAME_DATALENGTH * 2
 
-'''
-制作通信帧
-@addr   - 地址
-@cmd    - 指令
-@data   - 数据
-@return - 帧字符串
-'''
 def frame_make(addr, cmd, data):
+    ''' 制作通信帧\n @addr\t\t- 地址\n @cmd\t\t- 指令\n @data\t\t- 数据\n @return\t- 帧字符串 '''
     da = ['0' for _ in range(24)]
     for i, d in enumerate(data):
         split = char_split(d)
@@ -24,41 +18,24 @@ def frame_make(addr, cmd, data):
     frame = "{0}{1}{2}{3}".format(FRAME_HEAD, "".join(da), "".join(char_split(check)), FRAME_TAIL)
     return frame
     
-'''
-二字节拆分 - 拆分
-@char     - 一个字节
-@return   - 拆分后的两个字节
-'''
 def char_split(char):
+    ''' 二字节拆分\t- 拆分\n @char\t\t\t- 一个字节\n @return\t\t- 拆分后的两个字节 '''
     c = ord(char)
     return '{0}{1}'.format(hex(c / 16)[2:].upper(), hex(c % 16)[2:].upper())
 
-'''
-二字节拆分 - 组合
-@charH    - 高字节
-@charL    - 低字节
-@return   - 组合后的两个字节
-'''
 def char_combine(charH, charL):
+    ''' 二字节拆分\t- 组合\n @charH\t\t- 高字节\n @charL\t\t\t- 低字节\n @return\t\t- 组合后的两个字节\n '''
     return chr(int('{0}{1}'.format(charH, charL), base=16))
 
-'''
-计算校验和
-@data   - 数据
-@return - 校验和值
-'''
 def frame_check_sum(data):
+    ''' 计算校验和\n @data\t\t- 数据\n @return\t- 校验和值 '''
     check = 0
     for d in data:
         check = check + ord(d)
     return chr(check % 256)
 
-'''
-检查数据帧
-@frame  - 数据帧
-@return - 返回 地址，指令，数据
-'''
 def frame_check(frame):
+    ''' 检查数据帧\n @frame\t- 数据帧\n @return\t- 返回 地址，指令，数据 '''
     ''' e 错误输出 '''
     e = "[frameCheck] fail: "
 
@@ -78,12 +55,8 @@ def frame_check(frame):
     if not char_combine(frame[-3], frame[-2]) == frame_check_sum(frame[3:-3]):
         print e + "data check not correct!"
         return False, "E.CHECK"
-        
-    addr = ord(frame[3])
-    cmd  = ord(frame[4])
-    da = frame[5:-3]
+    addr, cmd, da = ord(frame[3]), ord(frame[4]), frame[5:-3]
     data = [char_combine(da[2 * i], da[2 * i + 1]) for i in range(len(da)/2)]
-        
     return True, (addr, cmd, data)
 
 
