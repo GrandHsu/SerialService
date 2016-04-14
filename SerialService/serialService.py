d6909481a782b084f66f1d2d0cb018025e76e704
rsy <mr.iridescent.rsy@hotmail.com>
2016-04-14 15:14:46 +0800

    添加详细注释

--- SerialService/serialService.py
+++ SerialService/serialService.py
@@ -26,7 +26,7 @@ class SerialService(object):
         self._serial = self.open_serial(self._serial_port, self._serial_baudrate)
 
     def open_serial(self, port, baudrate):
-        ''' 打开串口 '''
+        ''' 以 指定波特率 打开 指定串口\n @port\t\t\t- 端口号\n @baudrate\t- 波特率 '''
         try:
             print "Trying to open serial port: {0} @ {1} bps...".format(port, baudrate)
             _serial = serial.Serial(port, baudrate)
@@ -37,15 +37,19 @@ class SerialService(object):
             raise SystemExit("Quitting...")
 
     def close_serial(self):
+        ''' 关闭串口 '''
         self._serial.close()
 
     def write(self, addr, cmd, data):
+        ''' 发送帧\n @addr\t- 地址\n @cmd\t- 指令\n @data\t- 数据\n '''
         frame = cp.frame_make(addr, cmd, data)
         self._serial.write(frame)
 
     def read(self, timeout=0.1):
+        ''' 读取帧\n @timeout\t- 超时时间 '''
         # 设置超时
         self._serial.timeout = timeout
+        # 读取到帧尾字节
         frame = self._serial.read_until(cp.FRAME_TAIL)
         if len(frame) is 0:
             return False, "TIMEOUT"
@@ -81,6 +85,7 @@ class SerialService(object):
                 port = str(ports[p])
                 return (port.split(' '))[0]
 
+    '''
     #def communicate(self, addr, cmd, data, timeout=1):
     #     def handler(signum, frame):    
     #        raise AssertionError
@@ -94,15 +99,11 @@ class SerialService(object):
     #     except AssertionError:
     #         logger.error("communicate conn't finished in %d seconds, timeout!" % (timeout))
     #         return None
+    '''
 
     def communicate(self, addr, cmd, data, timeout=0.1):
-        '''
-        通信过程：
-        主机 -> 从机 frame
-        从机 处理 frame
-        从机 -> 主机 ack_frame 【有超时处理和错误处理】、【重发】（超时重发 & 出错重发）
-        主机 处理 ack
-        '''
+        ''' 完成发送接收一次通信\n @addr\t\t- 地址\n @cmd\t\t- 指令\n @data\t\t- 数据\n @timeout\t- 超时 '''
+        ''' TODO 通信过程：\n 主机 -> 从机 frame\n 从机 处理 frame\n 从机 -> 主机 ack_frame 【有超时处理和错误处理】、【重发】（超时重发 & 出错重发）\n 主机 处理 ack '''
         self.write(addr, cmd, data)
         return self.read(timeout)
 
